ce6d076145d89de7ec0c4aa41a24344b9c470a35
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 11:17:12 -0500

    Deployment from 0. correcting the routes structure.

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import FaqCard from './componets/5- FAQ/FAQ.tsx'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FAQPage from './pages/FAQPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <BrowserRouter>
       <Routes>
-          <FaqCard />
+      <Route path="/faq" element={<FAQPage />} />
       </Routes>    
       </BrowserRouter>
   </React.StrictMode>,
