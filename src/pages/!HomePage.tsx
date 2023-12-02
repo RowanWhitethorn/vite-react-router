9da817c9aef4815ffa07279bcf057f2bac2a72ca
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 13:58:49 -0500

    Deployment from 0. adding home page vacations.. servicios.. multimedia

--- src/pages/!HomePage.tsx
+++ src/pages/!HomePage.tsx
@@ -3,12 +3,13 @@ import { Vacaciones } from "../componets/3- Vacaciones/!Vacaciones";
 import { Servicios } from "../componets/4- Servicios/!Servicios";
 import { Footer } from "../componets/1- General/Footer";
 import { Home } from "../componets/2- Home/!Home";
-//import { ScrollingNavBar } from "../componets/1- General/ScrollingNavBar";
+import { ScrollingNavBar } from "../componets/1- General/ScrollingNavBar";
 
 function HomePage() {
   return (
     <>
       <div className="Homepage">
+        <ScrollingNavBar isHomePage={true} />
         <section className="section-home" id="home">
             <Home />
           </section>
