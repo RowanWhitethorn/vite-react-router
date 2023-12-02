b78857f7bc60cbc1371d5bec6e0e2a428738fe8e
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 11:25:14 -0500

    Deployment from 0. calling just the FAQ without using route path.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import FAQPage from './pages/FAQPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
