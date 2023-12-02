831fdb38533d5aefc67eef1bfb2b50af08f54dbd
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 11:29:36 -0500

    Deployment from 0. routing just the FaqCard without calling the FaqPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import { BrowserRouter, Routes } from 'react-router-dom'
-import FAQPage from './pages/FAQPage.tsx'
+//import FAQPage from './pages/FAQPage.tsx'
+import FaqCard from './componets/5- FAQ/FAQ.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <BrowserRouter>
-      <Routes>
-        <FAQPage/>
-      </Routes>    
+        <Routes>
+          <FaqCard />
+        </Routes>    
       </BrowserRouter>
   </React.StrictMode>,
 )
