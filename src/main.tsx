d2e2ead9958eaccfb8c1f21c8e77645938aac002
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 11:23:40 -0500

    Deployment from 0. calling just the FAQ without using route path

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <BrowserRouter>
       <Routes>
-      <Route path="/faq" element={<FAQPage />} />
+        <FAQPage/>
       </Routes>    
       </BrowserRouter>
   </React.StrictMode>,
