b83e6626768b9135eae0f820b221dce8f933a700
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-02 11:08:49 -0500

    Deployment from 0.adding react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import FaqCard from './componets/5- FAQ/FAQ.tsx'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <BrowserRouter>
+      <Routes>
           <FaqCard />
+      </Routes>    
+      </BrowserRouter>
   </React.StrictMode>,
 )
