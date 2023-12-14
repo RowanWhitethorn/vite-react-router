41f7a4a2b1d67c8405a59814cb637839e5e89ce6
RowanWhitethorn <joseantonio.diazv98@gmail.com>
2023-12-13 19:28:34 -0500

    Still deploy...

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 //import FAQPage from './pages/FAQPage.tsx'
 import HomePage from './pages/!HomePage.tsx'
 import FAQPage from './pages/FAQPage.tsx';
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter> 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/faq" element={<FAQPage />} />
-      </Routes>
-      </BrowserRouter>
+      <HashRouter> 
+
+        <HomePage />
+        <FAQPage />
+      </HashRouter>
   </React.StrictMode>,
 )
