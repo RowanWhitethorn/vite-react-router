5faa014103ce8f0d4b7ac694b72d3aed46175f37
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2023-12-14 16:20:01 -0500

    Trying again with Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 //import FAQPage from './pages/FAQPage.tsx'
 import HomePage from './pages/!HomePage.tsx'
 import FAQPage from './pages/FAQPage.tsx';
@@ -10,10 +10,11 @@ import FAQPage from './pages/FAQPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <HashRouter> 
-
-        <HomePage />
-        <FAQPage />
-      </HashRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/faq" element={<FAQPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
