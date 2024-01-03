f0ecc9e62c0b42fd08c349223103f31fe492db67
El_Pepe <88338187+RowanWhitethorn@users.noreply.github.com>
2024-01-03 10:02:02 -0500

    Trying to deploy with netlify through netlify..

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions:{
-      input:{
-        FAQPage: 'src/pages/FAQPage.tsx',
-        HomePage: 'src/pages/HomePage.tsx'
-      },
-    },
-  },
+
 });
 
